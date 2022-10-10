fb89a0203c038e1bf60ebd20573cd1693323d8a2
EvanaSabatella1989 <eva.sabatella@gmail.com>
2022-10-10 12:55:50 -0300

    Inicio Mi Portfolio en Angular

--- ProyectoAngular/src/app/app.component.ts
+++ ProyectoAngular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //el selector es el nombre de la etiqueta a la que referenciamos en el html
+  templateUrl: './app.component.html', //el template sera la vista
+  styleUrls: ['./app.component.css']  //style son los estilos
 })
 export class AppComponent {
   title = 'ProyectoAngular';
 }
+
+//el component es una clase, solo que se encuentra decorada con el @component como indicativo
--- ProyectoAngular/src/app/app.module.ts
+++ ProyectoAngular/src/app/app.module.ts
@@ -14,3 +14,10 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// el module es tambien una clase al igual que el Component, que solo esta decorado con el @NgModule que 
+//se encarga de indicarle a angular donde estan los bloques que necesita para su construccion, por ejemplo la 
+//declaracion de componentes o artefactos
+//los bloques que necesita importar
+//los provedores en caso que se necesite consumir algun servicio
+
